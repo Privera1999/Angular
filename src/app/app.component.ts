e75e4489e6fd5ce8cb2b047bb089c9ef900b7460
Pablo Rivera Jiménez <pabloriveraj1999@gmail.com>
2024-02-16 13:00:33 +0100

    Prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MiComponenteComponent} from "./mi-componente/mi-componente.component";
+
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MiComponenteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PruebasAngular11';
+  nombre:string = "Pablo Rivera"
 }
